5e6dabdc0aa3d15428f31ccc184d4a3469d2ad7f
Santiago <santiago.castrilllong@upb.edu.co>
2022-08-21 19:17:21 -0500

    Init Commit

---
+++ Assets/HttpManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class HttpManager : MonoBehaviour
+{
+    [SerializeField]
+    private string URL;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ClickGetScores()
+    {
+        StartCoroutine(GetScores());
+    }
+
+    IEnumerator GetScores()
+    {
+        string url = URL + "/leaders";
+        UnityWebRequest www = UnityWebRequest.Get(url);
+
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError)
+        {
+            Debug.Log("NETWORK ERROR " + www.error);
+        }
+        else if(www.responseCode == 200){
+            //Debug.Log(www.downloadHandler.text);
+            Scores resData = JsonUtility.FromJson<Scores>(www.downloadHandler.text);
+
+            foreach (ScoreData score in resData.scores)
+            {
+                //Debug.Log(score.userId +" | "+score.value);
+                LeaderboardManager.Instance.WriteScores(score.user_name, score.score);
+                Debug.Log("entre");
+            }
+        }
+        else
+        {
+            Debug.Log(www.error);
+        }
+    }
+   
+}
+
+
+[System.Serializable]
+public class ScoreData
+{
+    public string user_name;
+    public int score;
+
+}
+
+[System.Serializable]
+public class Scores
+{
+    public ScoreData[] scores;
+}
