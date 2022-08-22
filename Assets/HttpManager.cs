2d92f0a5d770f7922fffc5fc0b9633b9b4c9287c
Santiago <santiago.castrilllong@upb.edu.co>
2022-08-22 06:58:04 -0500

    Avance

--- Assets/HttpManager.cs
+++ Assets/HttpManager.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class HttpManager : MonoBehaviour
 {
     [SerializeField]
     private string URL;
+    [SerializeField] private InputField usernameUI;
+    [SerializeField] private InputField passwordUI;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +20,14 @@ public class HttpManager : MonoBehaviour
     {
         StartCoroutine(GetScores());
     }
+    public void ClickSignUp()
+    {
+        string username = usernameUI.text;
+        string password = passwordUI.text;
+        AuthData data = new AuthData(username, password);
+        string postData = JsonUtility.ToJson(data);
+        StartCoroutine(SignUp(postData));
+    }
 
     IEnumerator GetScores()
     {
@@ -25,7 +36,11 @@ public class HttpManager : MonoBehaviour
 
         yield return www.SendWebRequest();
 
-        if (www.isNetworkError)
+        //if (www.isNetworkError)
+        //{
+        //    Debug.Log("NETWORK ERROR " + www.error);
+        //}
+        if (www.result == UnityWebRequest.Result.ConnectionError)
         {
             Debug.Log("NETWORK ERROR " + www.error);
         }
@@ -45,7 +60,39 @@ public class HttpManager : MonoBehaviour
             Debug.Log(www.error);
         }
     }
-   
+    IEnumerator SignUp(string postData)
+    {
+
+        string url = URL + "/api/usuarios"; 
+        UnityWebRequest www = UnityWebRequest.Post(url,postData );
+
+        yield return www.SendWebRequest();
+
+        //if (www.isNetworkError)
+        //{
+        //    Debug.Log("NETWORK ERROR " + www.error);
+        //}
+        if (www.result == UnityWebRequest.Result.ConnectionError)
+        {
+            Debug.Log("NETWORK ERROR " + www.error);
+        }
+        else if (www.responseCode == 200)
+        {
+            //Debug.Log(www.downloadHandler.text);
+            Scores resData = JsonUtility.FromJson<Scores>(www.downloadHandler.text);
+
+            foreach (ScoreData score in resData.scores)
+            {
+                //Debug.Log(score.userId +" | "+score.value);
+                LeaderboardManager.Instance.WriteScores(score.user_name, score.score);
+                Debug.Log("entre");
+            }
+        }
+        else
+        {
+            Debug.Log(www.error);
+        }
+    }
 }
 
 
@@ -61,4 +108,17 @@ public class ScoreData
 public class Scores
 {
     public ScoreData[] scores;
+}
+
+[System.Serializable]
+public class AuthData
+{
+    public string username;
+    public string password;
+
+    public AuthData(string username, string password)
+    {
+        this.username = username;
+        this.password = password;
+    }
 }
